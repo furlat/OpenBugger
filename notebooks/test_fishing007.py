1649d2907251924d2e60a1ae1c826da9ffc78989
furlat <tfurlanello@gmail.com>
2023-01-10 22:05:39 +0100

    changed_regex, fixed fishing007 test cases

---
+++ notebooks/test_fishing007.py
@@ -0,0 +1,14 @@
+def test():
+    """my test lol
+    one two three"""
+    return "test"
+
+def two():
+    """ Returns 2 """
+    return 2
+
+def test2():
+    """
+    Test one tow
+    """
+    return None
